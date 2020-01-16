4e2424f177e23bf5b46b3a9d9ee18dee7856ff38
thakurups <umesh.singh_cs18@gla.ac.in>
2020-01-17 00:56:16 +0530

    creating a create quiz class to creat new quiz

---
+++ src/create/quiz/CreateQuiz.java
@@ -0,0 +1,14 @@
+package create.quiz;
+
+public class CreateQuiz {
+    int n;
+    int marksOfEach;
+    String quizName;
+
+    public CreateQuiz(int n, int marksOfEach, String quizName) {
+        this.n = n;
+        this.marksOfEach = marksOfEach;
+        this.quizName = quizName;
+    }
+
+}
