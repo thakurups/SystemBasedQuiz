c60fd45e1c516b8fd6bbcd3ad896f1178eb40d46
thakurups <umesh.singh_cs18@gla.ac.in>
2020-01-17 01:43:22 +0530

    fetchvalues method created

--- src/create/quiz/CreateQuiz.java
+++ src/create/quiz/CreateQuiz.java
@@ -1,14 +1,33 @@
 package create.quiz;
 
-public class CreateQuiz {
-    int n;
-    int marksOfEach;
-    String quizName;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Scanner;
 
-    public CreateQuiz(int n, int marksOfEach, String quizName) {
-        this.n = n;
-        this.marksOfEach = marksOfEach;
-        this.quizName = quizName;
+public class CreateQuiz {
+    int noOfQuestions;
+    String quizName;
+    int noAsked;
+
+    public void createTable(Connection c1) throws SQLException {
+        Connection connect = c1;
+        String query = "create table " + quizName + "(qid int,Question longtext,A mediumtext,B mediumtext,C mediumtext,D mediumtext,Ans mediumtext);";
+        PreparedStatement p = connect.prepareStatement(query);
+        p.executeUpdate();
+
     }
 
+    public void fetchValues() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the name of quiz you want to create");
+        quizName = sc.nextLine();
+        System.out.println("Enter the no of question you want to insert");
+        noOfQuestions = sc.nextInt();
+        System.out.println("Enter the no of question you want to ask");
+        noAsked = sc.nextInt();
+    }
 }
+
+
+
